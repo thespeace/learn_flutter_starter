0c45f1ad57115ded4da5d8c50584512b93a368a4
thespeace_notebook <thespeace1@gmail.com>
2024-02-11 15:04:43 +0900

    FEAT: Webtoon App Clone coding-waitForWebToons

--- lib/webtoon/screens/webtoon_home_screen.dart
+++ lib/webtoon/screens/webtoon_home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_starter/webtoon/services/webtoon_api_service.dart';
 
-class WebtoonHomeScreen extends StatelessWidget {
+import '../models/webtoon_model.dart';
+
+class WebtoonHomeScreen extends StatefulWidget {
   const WebtoonHomeScreen({super.key});
 
   @override
+  State<WebtoonHomeScreen> createState() => _WebtoonHomeScreenState();
+}
+
+class _WebtoonHomeScreenState extends State<WebtoonHomeScreen> {
+
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoons() async {
+    webtoons = await WebtoonApiService.getTodaysToons();
+    isLoading = false;
+    setState(() { });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
--- lib/webtoon/services/webtoon_api_service.dart
+++ lib/webtoon/services/webtoon_api_service.dart
@@ -5,10 +5,10 @@ import 'package:http/http.dart' as http;
 import '../models/webtoon_model.dart'; //패키지의 함수명이 너무 일반적이라 namespace를 지정.
 
 class WebtoonApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {//async(비동기 함수)에서만 await를 사용할 수 있고, 이는 요청을 완료하여 반환값을 받은 후에 다음 코드를 진행하라는 뜻.
+  static Future<List<WebtoonModel>> getTodaysToons() async {//async(비동기 함수)에서만 await를 사용할 수 있고, 이는 요청을 완료하여 반환값을 받은 후에 다음 코드를 진행하라는 뜻.
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');//baseUrl과 today로 endpoint API URL 생성.
     final response = await http.get(url);//return 타입인 Future<response>은 미래에 받을 값의 타입을 알려준다. await와 함께 사용해야하고 사용하면 response 변수는 그냥 Response 타입인 걸 확인 할 수 있다.
