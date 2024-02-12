f17f6134487ecfd6a1a110452e04d07c9110ec9c
thespeace_notebook <thespeace1@gmail.com>
2024-02-12 15:34:22 +0900

    FEAT: Webtoon App Clone coding-detail info

--- lib/webtoon/screens/webtoon_detail_screen.dart
+++ lib/webtoon/screens/webtoon_detail_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_starter/webtoon/services/webtoon_api_service.dart';
 
-class WebtoonDetailScreen extends StatelessWidget {
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
+
+class WebtoonDetailScreen extends StatefulWidget {
 
   final String title, thumb, id;
 
@@ -12,6 +16,22 @@ class WebtoonDetailScreen extends StatelessWidget {
   });
 
   @override
+  State<WebtoonDetailScreen> createState() => _WebtoonDetailScreenState();
+}
+
+class _WebtoonDetailScreenState extends State<WebtoonDetailScreen> {
+
+  late Future<WebtoonDetailModel> webtoon; //constructor에서 widget이 참조 될 수 없기때문에 late 선언.
+  late Future<List<WebtoonEpisodeModel>> episodes; //초기화 하고 싶은 property를 생성자에서 불가능한 경우, late를 유용하게 쓸 수 있다.
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = WebtoonApiService.getToonById(widget.id);
+    episodes = WebtoonApiService.getLatestEpisodeById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -20,7 +40,7 @@ class WebtoonDetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: Text(
-            title,
+            widget.title, //State의 build method가 State가 속한 StatefulWidget의 data를 받아오기 위해 widget을 명시해주어야 한다.
             style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w500,
@@ -30,13 +50,13 @@ class WebtoonDetailScreen extends StatelessWidget {
       body: Column(
         children: [
           const SizedBox(
-            height: 20,
+            height: 50,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Hero(
-                tag: id,
+                tag: widget.id,
                 child: Container(
                     width: 250,
                     clipBehavior: Clip.hardEdge, //자식의 부모 영역 침범을 제어하는 방법
@@ -51,15 +71,51 @@ class WebtoonDetailScreen extends StatelessWidget {
                         ]
                     ),
                     child: Image.network(
-                      thumb,
+                      widget.thumb,
                       headers: const {
                         'Referer': 'https://comic.naver.com',
                       },
-                    )
+                    ),
                 ),
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if(snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                    ),
+                    child: Column(
+                      crossAxisAlignment:  CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                return Text("...");
+            }
+          )
         ],
       ),
     );
